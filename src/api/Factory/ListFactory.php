18084c93af69aa2d312e45f587f20b1074d31c6b
Robert Grüning <r.gruening@web.de>
2020-05-28 18:01:08 +0200

    #80: Laden mittels Suchkriterien implementiert

--- src/api/Factory/ListFactory.php
+++ src/api/Factory/ListFactory.php
@@ -4,55 +4,38 @@ include_once(__DIR__."/IListFactory.php");
 
 class ListFactory implements iListFactory
 {
-    #region variables
-    private $_modelFactory = null;
-    #endregion
+	#region variables
+	private $_modelFactory = null;
+	#endregion
 
-    #region properties
-    protected function getModelFactory()
-    {
-        return $this->_modelFactory;
-    }
-    #endregion
-    
-    #region constructors
-    function __construct($modelFactory)
-    {
-        $this->_modelFactory = $modelFactory;
-    }
-    #endregion
+	#region properties
+	protected function getModelFactory()
+	{
+		return $this->_modelFactory;
+	}
+	#endregion
 
-    #region methods
-    #region load
-    public function loadAll()
-    {
-        $elements = array();
-        $mysqli = new mysqli(MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK);
-		
-		if (!$mysqli->connect_errno)
+	#region constructors
+	function __construct($modelFactory)
+	{
+		$this->_modelFactory = $modelFactory;
+	}
+	#endregion
+
+	#region methods
+	#region load
+	public function loadAll()
+	{
+		$searchConditions = array();
+		$elements = $this->getModelFactory()->loadBySearchConditions($searchConditions);
+
+		if ($elements == null)
 		{
-			$mysqli->set_charset("utf8");
-			$ergebnis = $mysqli->query($this->getSQLStatementToLoadAll());	
-			
-			if (!$mysqli->errno)
-			{
-				while ($datensatz = $ergebnis->fetch_assoc())
-				{
-					array_push($elements, $this->getModelFactory()->loadById(intval($datensatz["Id"])));
-				}
-			}
+			return array();
 		}
-		
-		$mysqli->close();
-		
+
 		return $elements;
-    }
-    
-    private function getSQLStatementToLoadAll()
-    {
-        return "SELECT Id
-                FROM ".$this->getModelFactory()->getTableName().";";
-    }
-    #endregion
-    #endregion
+	}
+	#endregion
+	#endregion
 }
