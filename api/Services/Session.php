88b7df9406b9aa7a85b95ec91ba0949e6ffc1cb9
Robert Grüning <robert.gruening@posteo.de>
2024-09-27 16:27:55 +0200

    Update Session.php
    Login ergänzt

--- api/Services/Session.php
+++ api/Services/Session.php
@@ -2,14 +2,46 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+require_once("../UserStories/User/LoadUsers.php");
+
 session_start();
 
 global $logger;
 
-if (isset($_POST) &&
-  	isset($_POST["logoff"])) {
+if (isset($_POST))
+	if (isset($_POST["logoff"])) {		
 		
-  	session_unset();
-  	session_destroy();
-  	return;
+        $logger->info("User-abmelden gestartet");
+		session_unset();
+		session_destroy();
+        $logger->info("User-abmelden beendet");
+		return;
+	}
+
+	if (isset($_POST["login"]) &&
+		isset($_POST["userName"])) {
+		
+        $logger->info("User-anmelden gestartet");
+        $loadUsers = new LoadUsers();
+
+        if ($loadUsers->run())
+        {
+			$users = $loadUsers->getUsers();
+			
+			foreach ($users as $user) {
+				if ($user->getUserName() == $_POST["userName"]) {
+					$_SESSION["UserGuid"] = $user->getGuid();
+					break;
+				}
+			}
+        }
+        else
+        {
+            http_response_code(500);
+            echo json_encode($loadUsers->getMessages());
+        }
+
+        $logger->info("User-anmelden beendet");
+		return;
+	}
 }
