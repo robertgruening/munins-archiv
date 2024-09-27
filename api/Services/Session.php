5ccc5b518c63983006b3c4d0d0e2c96ac447078e
Robert Grüning <robert.gruening@posteo.de>
2024-09-27 16:48:28 +0200

    Sessionabfrage erg.

--- api/Services/Session.php
+++ api/Services/Session.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+require_once("../UserStories/User/LoadUser.php");
 require_once("../UserStories/User/LoadUsers.php");
 
 session_start();
@@ -11,10 +12,10 @@ global $logger;
 if (isset($_POST))
 	if (isset($_POST["logoff"])) {		
 		
-        $logger->info("User-abmelden gestartet");
+		$logger->info("User-abmelden gestartet");
 		session_unset();
 		session_destroy();
-        $logger->info("User-abmelden beendet");
+		$logger->info("User-abmelden beendet");
 		return;
 	}
 
@@ -22,6 +23,8 @@ if (isset($_POST))
 		isset($_POST["userName"])) {
 		
         $logger->info("User-anmelden gestartet");
+
+		$userToLogin = null;
         $loadUsers = new LoadUsers();
 
         if ($loadUsers->run())
@@ -30,7 +33,7 @@ if (isset($_POST))
 			
 			foreach ($users as $user) {
 				if ($user->getUserName() == $_POST["userName"]) {
-					$_SESSION["UserGuid"] = $user->getGuid();
+					$userToLogin = $user;
 					break;
 				}
 			}
@@ -41,7 +44,50 @@ if (isset($_POST))
             echo json_encode($loadUsers->getMessages());
         }
 
+		if ($userToLogin == null) {
+            http_response_code(401);
+            echo "[ \"Anmeldung fehlgeschlagen!\" ]";
+		}
+		else {
+			$_SESSION["UserGuid"] = $userToLogin->getGuid();
+			$loadUser = new LoadUser();
+			$loadUser->setGuid($userToLogin->getGuid());
+			
+			if ($loadUser->run())
+			{
+				echo json_encode($loadUser->getUser());
+			}
+			else
+			{
+				http_response_code(500);
+				echo json_encode($loadUser->getMessages());
+			}
+		}
+
         $logger->info("User-anmelden beendet");
 		return;
 	}
+}
+else if (isset($_GET)) {
+	$logger->info("Session-abfragen gestartet");
+
+	if (!isset($_SESSION["UserGuid"])) {
+		http_response_code(403);
+		return;
+	}
+
+	$loadUser = new LoadUser();
+	$loadUser->setGuid($_SESSION["UserGuid"]);
+	
+	if ($loadUser->run())
+	{
+		echo json_encode($loadUser->getUser());
+	}
+	else
+	{
+		http_response_code(500);
+		echo json_encode($loadUser->getMessages());
+	}
+	
+	$logger->info("Session-abfragen beendet");
 }
