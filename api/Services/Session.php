02f03bc79734128a51c70b99a6a6d2688f0d4923
Robert Grüning <robert.gruening@posteo.de>
2024-09-27 16:56:57 +0200

    extract method

--- api/Services/Session.php
+++ api/Services/Session.php
@@ -11,64 +11,72 @@ global $logger;
 
 if (isset($_POST))
 	if (isset($_POST["logoff"])) {		
-		
-		$logger->info("User-abmelden gestartet");
-		session_unset();
-		session_destroy();
-		$logger->info("User-abmelden beendet");
-		return;
+		logoff();
 	}
-
-	if (isset($_POST["login"]) &&
-		isset($_POST["userName"])) {
-		
-        $logger->info("User-anmelden gestartet");
-
-		$userToLogin = null;
-        $loadUsers = new LoadUsers();
-
-        if ($loadUsers->run())
-        {
-			$users = $loadUsers->getUsers();
-			
-			foreach ($users as $user) {
-				if ($user->getUserName() == $_POST["userName"]) {
-					$userToLogin = $user;
-					break;
-				}
-			}
-        }
-        else
-        {
-            http_response_code(500);
-            echo json_encode($loadUsers->getMessages());
-        }
-
-		if ($userToLogin == null) {
-            http_response_code(401);
-            echo "[ \"Anmeldung fehlgeschlagen!\" ]";
-		}
-		else {
-			$_SESSION["UserGuid"] = $userToLogin->getGuid();
-			$loadUser = new LoadUser();
-			$loadUser->setGuid($userToLogin->getGuid());
-			
-			if ($loadUser->run())
-			{
-				echo json_encode($loadUser->getUser());
-			}
-			else
-			{
-				http_response_code(500);
-				echo json_encode($loadUser->getMessages());
-			}
-		}
-
-        $logger->info("User-anmelden beendet");
-		return;
+	else if (isset($_POST["login"]) {
+		login();
 	}
 }
 else if (isset($_GET)) {
+	getSessionState();
+}
+
+function logoff() {
+	$logger->info("User-abmelden gestartet");
+	session_unset();
+	session_destroy();
+	$logger->info("User-abmelden beendet");
+}
+
+function login() {
+	$logger->info("User-anmelden gestartet");
+		
+	if (!isset($_POST["userName"])) {
+		http_response_code(401);
+		return;
+	}		
+
+	$userByUserName = null;
+	$loadUsers = new LoadUsers();
+
+	if ($loadUsers->run())
+	{
+		$users = $loadUsers->getUsers();
+		
+		foreach ($users as $user) {
+			if ($user->getUserName() == $_POST["userName"]) {
+				$userByUserName = $user;
+				break;
+			}
+		}
+	}
+	else
+	{
+		http_response_code(401);
+	}
+
+	if ($userByUserName == null) {
+		http_response_code(401);
+	}
+	else {
+		$_SESSION["UserGuid"] = $userByUserName->getGuid();
+		$loadUser = new LoadUser();
+		$loadUser->setGuid($userByUserName->getGuid());
+		
+		if ($loadUser->run())
+		{
+			echo json_encode($loadUser->getUser());
+		}
+		else
+		{
+			http_response_code(401);
+		}
+	}
+
+	$logger->info("User-anmelden beendet");
+}
+
+function getSession() {
 	$logger->info("Session-abfragen gestartet");
 
 	if (!isset($_SESSION["UserGuid"])) {
