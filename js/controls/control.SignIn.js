f5da92405f24244e7e7f04e7f8d4add4d66cc31f
Robert Grüning <r.gruening@web.de>
2024-10-04 00:35:02 +0200

    Lesezeichen setzen vorbereitet

--- js/controls/control.SignIn.js
+++ js/controls/control.SignIn.js
@@ -40,6 +40,18 @@ function setUserSignedInState(user) {
     $("#navigation-item-sign-off").removeClass("disabled");
     $("#navigation-item-sign-off").attr("href", "javascript:void(0)");
     $("#navigation-item-sign-off").attr("onclick", "signOff();");
+
+    $("#navigation-item-set-bookmark").removeClass("disabled");
+    $("#navigation-item-set-bookmark").attr("href", "javascript:void(0)");
+    $("#navigation-item-set-bookmark").attr("onclick", "setBookmark();");
+
+    if (user.Bookmark != null &&
+        user.Bookmark.length >= 1) {
+
+        $("#navigation-item-open-bookmark").removeClass("disabled");
+        $("#navigation-item-open-bookmark").attr("href", "javascript:void(0)");
+        $("#navigation-item-open-bookmark").attr("onclick", "openBookmark();");
+    }
 }
 
 function setUserSignedOffState() {
@@ -49,4 +61,12 @@ function setUserSignedOffState() {
     $("#navigation-item-sign-off").addClass("disabled");
     $("#navigation-item-sign-off").removeAttr("href");
     $("#navigation-item-sign-off").removeAttr("onclick");
+
+    $("#navigation-item-set-bookmark").addClass("disabled");
+    $("#navigation-item-set-bookmark").removeAttr("href");
+    $("#navigation-item-set-bookmark").removeAttr("onclick");
+
+    $("#navigation-item-open-bookmark").addClass("disabled");
+    $("#navigation-item-open-bookmark").removeAttr("href");
+    $("#navigation-item-open-bookmark").removeAttr("onclick");
 }
